2b2b0565b37228f45a7b6ce54fa0d39c520f7405
SStamenov <stefan.stamenov1996@gmail.com>
2021-06-10 20:50:45 +0300

    Add implementation

--- sort_skeleton/sort/sorting-service/service.go
+++ sort_skeleton/sort/sorting-service/service.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"math/rand"
-	"reflect"
 
 	"github.com/Stamenov96/GoLang101/sort/gen"
 )
@@ -13,37 +12,36 @@ func newSortingService() gen.SortingRobotServer {
 	return &sortingService{}
 }
 
-var currentItems []gen.Item
-var selectedItem gen.Item
-
-type sortingService struct{}
+type sortingService struct {
+	currentItems []gen.Item
+	selectedItem *gen.Item
+}
 
 func (s *sortingService) LoadItems(ctx context.Context, in *gen.LoadItemsRequest) (*gen.LoadItemsResponse, error) {
 	for _, item := range in.GetItems() {
-		currentItems = append(currentItems, *item)
+		s.currentItems = append(s.currentItems, *item)
 	}
 	return &gen.LoadItemsResponse{}, nil
 }
 
 func (s *sortingService) SelectItem(context.Context, *gen.SelectItemRequest) (*gen.SelectItemResponse, error) {
 
-	if len(currentItems) <= 0 {
+	if len(s.currentItems) <= 0 {
 		return nil, errors.New("no items in the input bin")
 	}
 
-	randomIndex := rand.Intn(len(currentItems))
-	selectedItem = currentItems[randomIndex]
-	currentItems = append(currentItems[:randomIndex], currentItems[randomIndex+1:]...)
-
-	return &gen.SelectItemResponse{Item: &selectedItem}, nil
+	randomIndex := rand.Intn(len(s.currentItems))
+	s.selectedItem = &s.currentItems[randomIndex]
+	s.currentItems = append(s.currentItems[:randomIndex], s.currentItems[randomIndex+1:]...)
+	return &gen.SelectItemResponse{Item: s.selectedItem}, nil
 }
 
 func (s *sortingService) MoveItem(ctx context.Context, in *gen.MoveItemRequest) (*gen.MoveItemResponse, error) {
 
-	if reflect.ValueOf(selectedItem).IsZero() {
+	if s.selectedItem == nil {
 		return nil, errors.New("there is no selected item")
 	}
 
-	selectedItem = gen.Item{}
+	*s.selectedItem = gen.Item{}
 	return &gen.MoveItemResponse{}, nil
 }
