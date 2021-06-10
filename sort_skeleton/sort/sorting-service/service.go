c392dca9f6a58131f51f90787fe461d74f0a674f
SStamenov <stefan.stamenov1996@gmail.com>
2021-06-10 13:08:24 +0300

    Implementing sortingService methods

--- sort_skeleton/sort/sorting-service/service.go
+++ sort_skeleton/sort/sorting-service/service.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"errors"
+	"math/rand"
+	"reflect"
 
 	"github.com/Stamenov96/GoLang101/sort/gen"
 )
@@ -11,19 +13,37 @@ func newSortingService() gen.SortingRobotServer {
 	return &sortingService{}
 }
 
+var currentItems []gen.Item
+var selectedItem gen.Item
+
 type sortingService struct{}
 
-func (s *sortingService) LoadItems(context.Context, *gen.LoadItemsRequest) (*gen.LoadItemsResponse, error) {
-	// TODO: Implement this
-	return nil, errors.New("not implemented")
+func (s *sortingService) LoadItems(ctx context.Context, in *gen.LoadItemsRequest) (*gen.LoadItemsResponse, error) {
+	for _, item := range in.GetItems() {
+		currentItems = append(currentItems, *item)
+	}
+	return &gen.LoadItemsResponse{}, nil
 }
 
 func (s *sortingService) SelectItem(context.Context, *gen.SelectItemRequest) (*gen.SelectItemResponse, error) {
-	// TODO: Implement this
-	return nil, errors.New("not implemented")
+
+	if len(currentItems) <= 0 {
+		return nil, errors.New("no items in the input bin")
+	}
+
+	randomIndex := rand.Intn(len(currentItems))
+	selectedItem = currentItems[randomIndex]
+	currentItems = append(currentItems[:randomIndex], currentItems[randomIndex+1:]...)
+
+	return &gen.SelectItemResponse{Item: &selectedItem}, nil
 }
 
-func (s *sortingService) MoveItem(context.Context, *gen.MoveItemRequest) (*gen.MoveItemResponse, error) {
-	// TODO: Implement this
-	return nil, errors.New("not implemented")
+func (s *sortingService) MoveItem(ctx context.Context, in *gen.MoveItemRequest) (*gen.MoveItemResponse, error) {
+
+	if reflect.ValueOf(selectedItem).IsZero() {
+		return nil, errors.New("there is no selected item")
+	}
+
+	selectedItem = gen.Item{}
+	return &gen.MoveItemResponse{}, nil
 }
