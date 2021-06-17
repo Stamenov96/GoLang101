d3f003c6fcf50377961f127c929f4bd51d284544
SStamenov <stefan.stamenov1996@gmail.com>
2021-06-17 17:23:50 +0300

    Adding another test

--- sort_skeleton/sort/sorting-service/service_test.go
+++ sort_skeleton/sort/sorting-service/service_test.go
@@ -22,3 +22,49 @@ func TestLoadItems(t *testing.T) {
 	assert.Equal(t, err, nil, "there should be no error")
 
 }
+
+func TestSelectItem(t *testing.T) {
+
+	sorting_service := newSortingService()
+
+	load_request := &gen.LoadItemsRequest{
+		Items: []*gen.Item{&gen.Item{}, &gen.Item{}},
+	}
+	sorting_service.LoadItems(context.Background(), load_request)
+
+	res, err := sorting_service.SelectItem(context.Background(), &gen.SelectItemRequest{})
+
+	assert.NotEqual(t, res, nil, "result should be empty SelectItemsResult")
+	assert.Equal(t, err, nil, "there should be no error")
+
+}
+
+func TestSelectItemWithEmptyBin(t *testing.T) {
+
+	assert := assert.New(t)
+	sorting_service := newSortingService()
+
+	res, err := sorting_service.SelectItem(context.Background(), &gen.SelectItemRequest{})
+	assert.Nil(res, "Result should be empty")
+	assert.NotNil(err, "There should be no error")
+}
+
+func TestMoveItem(t *testing.T) {
+
+	sorting_service := newSortingService()
+
+	//  Load
+	load_request := &gen.LoadItemsRequest{
+		Items: []*gen.Item{&gen.Item{}, &gen.Item{}},
+	}
+	sorting_service.LoadItems(context.Background(), load_request)
+
+	// Select
+	sorting_service.SelectItem(context.Background(), &gen.SelectItemRequest{})
+
+	res, err := sorting_service.MoveItem(context.Background(), &gen.MoveItemRequest{})
+
+	assert.NotEqual(t, res, nil, "result should be empty SelectItemsResult")
+	assert.Equal(t, err, nil, "there should be no error")
+
+}
